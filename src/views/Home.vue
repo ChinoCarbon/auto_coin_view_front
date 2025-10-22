afda2e4c5c610ccf3e7fdbb4cde231d9c9c399b5
Re-AutoLightApk <466310196@qq.com>
2025-10-23 00:19:19 +0800

    版本号忘改了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -75,7 +75,7 @@
 
     <n-layout-footer class="footer-container">
       <div class="footer-content">
-        <span class="footer-title">动态表格 V6.5</span>
+        <span class="footer-title">动态表格 V6.6(下单测试版)</span>
       </div>
     </n-layout-footer>
     
